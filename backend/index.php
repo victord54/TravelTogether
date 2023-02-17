b96bc1d631981257081cdd3799c8bcc7a17bcceb
Victor <dallevictor@gmail.com>
2023-02-17 17:09:26 +0100

    Snippet pour faire fonctionner l'API. Appel à l'API impossible 'Access-Control-Allow-Origin'

--- backend/index.php
+++ backend/index.php
@@ -1,8 +1,9 @@
 <?php
-
-echo json_encode([
+$data = [
     'title' => 'Cassoulet',
     'recipe' => 'Etape 1 : des flageolets, Etape 2 : ...',
     'author' => 'john.doe@exemple.com',
     'enabled' => true,
-]);
+];
+header('Content-Type: application/json; charset=utf-8; Access-Control-Allow-Origin: *');
+echo json_encode($data);
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react";
+
 function Login() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect( () => {
+        fetch("http://travel-together/")
+        .then( (res) => res.json())
+        .then(
+            (result) => {
+                setIsLoaded(true);
+                setItems(result.results);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        );
+    }, []);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item.title}</li>
+                ))}
+            </ul>
+        )
+    }
+
     return (<div>Hello</div>);
 }
 
