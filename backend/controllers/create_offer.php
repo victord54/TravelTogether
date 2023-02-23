3341a1b2e994c66bacd001694b107fdf79bea41c
Di3bold <erwandiebold54@gmail.com>
2023-02-23 19:50:56 +0100

    Prise en compte des groupes dirigé

--- backend/controllers/create_offer.php
+++ backend/controllers/create_offer.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     $pdo = new PDO('mysql:host=localhost;dbname=travel_together;charset=utf8', 'travel_together', 'travel_together');
-    $statement = $pdo->prepare("SELECT DISTINCT(idfGroupe), nomDeGroupe FROM APPARTIENT JOIN GROUPE USING(idfGroupe) WHERE email = :mail");
+    $statement = $pdo->prepare("SELECT idfGroupe, nomDeGroupe FROM APPARTIENT JOIN GROUPE USING(idfGroupe) WHERE email = :mail UNION SELECT idfGroupe, nomDeGroupe FROM GROUPE WHERE dirigeant = :mail");
     $statement->setFetchMode(PDO::FETCH_ASSOC);
     $statement->bindValue(":mail", $_GET['mail']);
     $statement->execute();
