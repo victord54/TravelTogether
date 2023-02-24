d22d6964536265a520c2db075d285833d63b1246
Di3bold <erwandiebold54@gmail.com>
2023-02-24 18:51:49 +0100

    Correction login

--- backend/controllers/login.php
+++ backend/controllers/login.php
@@ -1,5 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
+    header("Content-type:application/json");
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
