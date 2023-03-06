d3f6e4ad98ce4e12d0407a151d31f9bded05fe44
Di3bold <erwandiebold54@gmail.com>
2023-03-06 12:21:22 +0100

    affichage des informations utiles

--- frontend/src/components/Offer.jsx
+++ frontend/src/components/Offer.jsx
@@ -1,6 +1,12 @@
-import {useEffect, useState, useRef} from 'react';
-
 function Offer(data) {
+    var info = <></>;
+    var precision = <></>;
+
+    if(data["infos"].length > 1) info = (<main><h3>Informations :</h3>
+    <p>{data["infos"]}</p></main>);
+    if(data["precisions"].length > 1) precision = (<main><h3>Précision :</h3>
+    <p>{data["precisions"]}</p></main>);
+
     if(data["inter"] && data["inter"].length > 0)
         return (
         <section key={data["idfOffre"]}>
@@ -8,13 +14,11 @@ function Offer(data) {
             <h3>{data["nom"] + " " + data["prenom"]}</h3>
             <p>{data["dateDepart"] + " " + data["heureDepart"]}</p>
             <h3>Arrêts intermédiaires :</h3>
-            <ul>
+            <ul className='citiesList'>
                 {data["inter"].map((city, index) => <ul key={index}>{city}</ul>)}
             </ul>
-            <h3>Précision :</h3>
-            <p>{data["precisions"]}</p>
-            <h3>Informations :</h3>
-            <p>{data["infos"]}</p>
+            {precision}
+            {info}
         </section>);
     else
         return (
@@ -22,10 +26,8 @@ function Offer(data) {
                 <h2>{data["villeDepart"] + " -> " + data["villeArrivee"]}</h2>
                 <h3>{data["nom"] + " " + data["prenom"]}</h3>
                 <p>{data["dateDepart"] + " " + data["heureDepart"]}</p>
-                <h3>Précision :</h3>
-                <p>{data["precisions"]}</p>
-                <h3>Informations :</h3>
-                <p>{data["infos"]}</p>
+                {precision}
+                {info}
             </section>);
 };
 
