d4fa7fb32f49246f7278f7f075524a99ddbae0f1
Nathan <nathan.faynot3@etu.univ-lorraine.fr>
2023-03-07 21:35:10 +0100

    Ajout d'un membre possible

--- backend/controllers/addmember_group.php
+++ backend/controllers/addmember_group.php
@@ -1,34 +1,33 @@
 <?php
 include 'header.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $pdo = new PDO('mysql:host=localhost;dbname=travel_together;charset=utf8', $login, $password);
-    $statement = $pdo->prepare("DELETE FROM GROUPE WHERE idfGroupe = :idfGroupe AND dirigeant = :email");
-    $statement->setFetchMode(PDO::FETCH_ASSOC);
+if($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+  $pdo = new PDO('mysql:host=localhost;dbname=travel_together;charset=utf8', $login, $password);
+  $statement = $pdo->prepare("INSERT INTO APPARTIENT(email, idfGroupe) values (:email, :idfGroupe)");
+  $statement->setFetchMode(PDO::FETCH_ASSOC);
 
-    $statement->bindValue(":idfGroupe", $_POST["idfGroupe"]);
-    $statement->bindValue(":email", $_POST["mail"]);
+  $statement->bindValue(":idfGroupe", $_POST["idfGroupe"]);
+  $statement->bindValue(":email", $_POST["email"]);
 
-    $statement->execute() or die(print_r($statement->errorInfo(), true));
-    echo "1";
+  $statement->execute() or die(print_r($statement->errorInfo(), true));
+  echo "1";
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $pdo = new PDO('mysql:host=localhost;dbname=travel_together;charset=utf8', $login, $password);
-    $statement = $pdo->prepare("SELECT motDePasse FROM UTILISATEUR WHERE email = :mail");
-    $statement->setFetchMode(PDO::FETCH_ASSOC);
-    $statement->bindValue(":mail", $_GET['mail']);
-    $statement->execute() or die(print_r($statement->errorInfo(), true));
-    $data = $statement->fetch();
+if($_SERVER['REQUEST_METHOD'] === 'GET')
+{
+  $pdo = new PDO('mysql:host=localhost;dbname=travel_together;charset=utf8', $login, $password);
+  $statement = $pdo->prepare("SELECT email FROM UTILISATEUR WHERE email = :email");
+  $statement->setFetchMode(PDO::FETCH_ASSOC);
+  $statement->bindValue(":email", $_GET['email']);
+  $statement->execute() or die(print_r($statement->errorInfo(), true));
+  $data = $statement->fetch();
 
-    $reponse = null;
-    if ($data) {
-        if (password_verify($_GET['password'], $data['motDePasse'])) {
-            $reponse = '1';
-        } else {
-            $reponse = '0';
-        }
-    }
+  $reponse = '0';
+  if($data)
+  {
+    $reponse = '1';
+  }
 
-    echo json_encode($reponse);
+  echo json_encode($reponse);
 }
