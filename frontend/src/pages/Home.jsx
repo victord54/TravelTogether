48f5eb86119686fa357b9634eb4d6ac66c71de7b
Victor Dallé <dallevictor@gmail.com>
2023-03-08 23:44:00 +0100

    Ajout message sur page non connecté

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,28 @@
 import "../styles/Home.css";
-import {useState} from "react";
-import {url_api} from "../data/url_api";
-import {useAuth} from "../components/AuthProvider";
+import { useState } from "react";
+import { url_api } from "../data/url_api";
+import { useAuth } from "../components/AuthProvider";
 import axios from "axios";
 import Offer from "../components/Offer";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Home() {
     const { auth } = useAuth();
-    const [ offres, setOffres ] = useState({offres : [], statut : ""});
-    
+    const [offres, setOffres] = useState({ offres: [], statut: "" });
+
     async function getOffres() {
         await axios
             .get(url_api.url + "/offer", {
                 params: {
-                    email : localStorage.getItem("mail")
-                }
+                    email: localStorage.getItem("mail"),
+                },
             })
             .then(function (reponse) {
                 if (reponse.data == null) {
-                    setOffres({offres : [], statut : "ok" });
+                    setOffres({ offres: [], statut: "ok" });
                 } else {
-                    setOffres({offres : reponse.data, statut : "ok" });
+                    setOffres({ offres: reponse.data, statut: "ok" });
+                    console.log(reponse.data);
                 }
             })
             .catch(function (error) {
@@ -29,39 +30,51 @@ function Home() {
             });
     }
 
-    if(!auth) {
-        return <main>
-            <p>Bienvenue !</p>
-        </main>
-    } else if(offres.statut === "") {
-        getOffres();
-    } else if(offres.statut === "ok" && offres.offres.length === 0)
-        return(
+    if (!auth) {
+        return (
             <main>
-            {auth ? (
+                <div id="not_connected">
+                    <h1 id="title_not_connected">Bienvenue !</h1>
+                    <p id="home_not_connected">
+                        Inscrivez-vous pour profiter d'un covoiturage
+                    </p>
+                </div>
+            </main>
+        );
+    } else if (offres.statut === "") {
+        getOffres();
+    } else if (offres.statut === "ok" && offres.offres.length === 0)
+        return (
+            <main>
+                {auth ? (
+                    <p>
+                        Bienvenue {localStorage.getItem("nom")}{" "}
+                        {localStorage.getItem("prenom")} !
+                    </p>
+                ) : (
+                    <p> Bienvenue !</p>
+                )}
+                <article>
+                    <p>Aucune offre disponible</p>
+                </article>
+            </main>
+        );
+    else
+        return (
+            <main>
                 <p>
                     Bienvenue {localStorage.getItem("nom")}{" "}
                     {localStorage.getItem("prenom")} !
                 </p>
-            ) : (
-                <p> Bienvenue !</p>
-            )}
-            <article>
-                <p>Aucune offre disponible</p>
-            </article>
-        </main>
-        )
-    else return (
-        <main>
-            <p>
-                Bienvenue {localStorage.getItem("nom")}{" "}
-                {localStorage.getItem("prenom")} !
-            </p>
-            <article>
-                {offres.offres.map((offre, index) => <Link to={"../offre/" + offre["idfOffre"]} key={index}>{Offer(offre)}</Link>)}
-            </article>
-        </main>
-    );
+                <article>
+                    {offres.offres.map((offre, index) => (
+                        <Link to={"../offre/" + offre["idfOffre"]} key={index}>
+                            {Offer(offre)}
+                        </Link>
+                    ))}
+                </article>
+            </main>
+        );
 
     function isJson(str) {
         try {
@@ -72,6 +85,5 @@ function Home() {
         return true;
     }
 }
-
 
 export default Home;
