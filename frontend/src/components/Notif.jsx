9f11166cd66a509c7e7e1952dd6e61138b89de01
Claire <claire.kurth@orange.fr>
2023-03-08 22:45:06 +0100

    css notifications

--- frontend/src/components/Notif.jsx
+++ frontend/src/components/Notif.jsx
@@ -3,8 +3,8 @@ import "../styles/Notif.css";
 function Notif(data) {
     return (
         <section className = {data.className}>
-            <h2>{data.type}</h2>
-            <p>{data.date}</p>
+            <p className="titre">{data.titre}</p>
+            <p className="date">{data.date}</p>
             <p>{data.message}</p>
         </section>
     );
