25c8f153b57a46783f2fb1627ec3db36a33ca222
Victor <dallevictor@gmail.com>
2023-03-14 11:56:20 +0100

    Correction bug nommage table (again)

--- backend/controllers/grade_participants.php
+++ backend/controllers/grade_participants.php
@@ -4,7 +4,7 @@ include 'header.php';
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $pdo = new PDO('mysql:host=localhost;dbname=travel_together;charset=utf8', $login, $password);
 
-    $statement = $pdo->prepare("SELECT * from UTILISATEUR JOIN NOTIFICATION WHERE idfOffre = :id AND statutReponse = 'accepter' AND (utilisateur.email = notification.interesse OR utilisateur.email = notification.notifie);");
+    $statement = $pdo->prepare("SELECT * from UTILISATEUR JOIN NOTIFICATION WHERE idfOffre = :id AND statutReponse = 'accepter' AND (UTILISATEUR.email = notification.interesse OR UTILISATEUR.email = notification.notifie);");
     $statement->setFetchMode(PDO::FETCH_ASSOC);
     $statement->bindValue(":id", $_GET['id']);
     $statement->execute();
