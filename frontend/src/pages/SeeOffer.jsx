fce7e30c0c56e8c958b7d38d91d44a0e1309a3a6
Di3bold <erwandiebold54@gmail.com>
2023-03-17 10:01:35 +0100

    On affiche plus le formulaire si offre déjà demandé, disparition des offres privées déjà passée sur la page d'accueil

--- frontend/src/pages/SeeOffer.jsx
+++ frontend/src/pages/SeeOffer.jsx
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import axios from 'axios';
 import {url_api} from "../data/url_api";
 import Offer from "../components/Offer";
-import {useParams, Link} from "react-router-dom";
+import {useParams, Link, useNavigate} from "react-router-dom";
 import "../styles/SeeOffer.css";
 import "../styles/NavButton.css";
 
@@ -16,6 +16,7 @@ function SeeOffer() {
     const [offers, setOffers] = useState({statut : "", offer : null});
     const [formValues, setInputValues] = useState(initialValue);
     const [error, setError] = useState(null);
+    const navigate = useNavigate();
     var buttons = <nav className='navButton'></nav>;
 
     function load_data() { 
@@ -25,6 +26,7 @@ function SeeOffer() {
             }
         })
             .then(function (response) {
+                console.log(response.data);
                 setOffers({statut : "ok", offer : response.data});
             })
             .catch(function (err) {
@@ -62,7 +64,7 @@ function SeeOffer() {
                         setError("Il n'y pas assez de places disponibles pour cette offre.")
                     }
                 }else{
-                    setError(null);
+                    navigate("/offre/" + id);
                 }
             })
             .catch(function (error) {
@@ -95,23 +97,45 @@ function SeeOffer() {
             date.getMonth() === ajd.getMonth() && 
             date.getFullYear() === ajd.getFullYear())
             && offers.offer["nbPlaceDisponible"] > 0) {
-            for(let i = 1; i<=offers.offer["nbPlaceDisponible"]; i++) places.push(<option key={i} value={i}>{i}</option>);
-            form = <form onSubmit={handleSubmit}>
-            <div>Nombre de places souhaitées :</div>
-            <label>
-                <select name="nbPlaces" id="nbPlaces" value={formValues.nbPlacesSouhaitees} onChange={handleChange}>
-                    {places}
-                </select>
-            </label>
-            <div>Message (facultatif) :</div>
-            <label>
-            <textarea name="messageFalcutatif" id="messageFalcutatif" value={formValues.messageFalcutatif} onChange={handleChange}></textarea>
-            </label>
-            <div className='button-forms-wrap'>
-                <button type="submit" className="formulaire-submit">Répondre à l'offre</button>
-            </div>
-            <p className="error">{error}</p>
-        </form>;
+
+
+                function email_equals(obj1, email) {
+                    return obj1["email"] = email;
+                }
+                function search_reponses(array, email) {
+                    for(var i = 0; i < array.length; i++) {
+                        if (email_equals(array[i], email)) return i;
+                    }
+                    return -1;
+                }
+                let index = search_reponses(offers.offer["reponses"], localStorage.getItem("mail"));
+
+                if(index == -1) {
+                    console.log(offers.offer["reponses"]);
+                    for(let i = 1; i<=offers.offer["nbPlaceDisponible"]; i++) places.push(<option key={i} value={i}>{i}</option>);
+                    
+                    form = <form onSubmit={handleSubmit}>
+                        <div>Nombre de places souhaitées :</div>
+                        <label>
+                            <select name="nbPlaces" id="nbPlaces" value={formValues.nbPlacesSouhaitees} onChange={handleChange}>
+                                {places}
+                            </select>
+                        </label>
+                        <div>Message (facultatif) :</div>
+                        <label>
+                        <textarea name="messageFalcutatif" id="messageFalcutatif" value={formValues.messageFalcutatif} onChange={handleChange}></textarea>
+                        </label>
+                        <div className='button-forms-wrap'>
+                            <button type="submit" className="formulaire-submit">Répondre à l'offre</button>
+                        </div>
+                        <p className="error">{error}</p>
+                    </form>;
+                } else {
+                    let statut = offers.offer["reponses"][index]["statutReponse"];
+                    if(statut != 'refuser') {
+                        // Code de Dan
+                    }
+                }
         }
     }
     let ajd = new Date();
