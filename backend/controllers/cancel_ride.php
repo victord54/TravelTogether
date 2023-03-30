59e63ee1205456fa8ff6b617df60a84f775bfe73
NathanFaynot <91254175+NathanFaynot@users.noreply.github.com>
2023-03-30 11:47:54 +0200

    Update cancel_ride.php

--- backend/controllers/cancel_ride.php
+++ backend/controllers/cancel_ride.php
@@ -4,31 +4,20 @@ include 'header.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $pdo = new PDO('mysql:host=localhost;dbname=travel_together;charset=utf8', $login, $password);
     $statement = $pdo->prepare("UPDATE NOTIFICATION SET statutReponse = 'annuler' WHERE idfOffre = :idfOffre AND interesse = :mail");
-
     $statement->bindValue(":idfOffre", $_POST["idfOffre"]);
     $statement->bindValue(":mail", $_POST["mail"]);
-
     $statement->execute() or die(print_r($statement->errorInfo(), true));
 
-    echo "1";
-}
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $pdo = new PDO('mysql:host=localhost;dbname=travel_together;charset=utf8', $login, $password);
-    $statement = $pdo->prepare("SELECT motDePasse FROM UTILISATEUR WHERE email = :mail");
-    $statement->setFetchMode(PDO::FETCH_ASSOC);
-    $statement->bindValue(":mail", $_GET['mail']);
+    $statement = $pdo->prepare("SELECT nbPlaceSouhaitees FROM NOTIFICATION WHERE idOffre = :idOffre");
+    $statement->bindValue(":idfOffre", $_POST["idfOffre"]);
     $statement->execute() or die(print_r($statement->errorInfo(), true));
     $data = $statement->fetch();
 
-    $reponse = null;
-    if ($data) {
-        if (password_verify($_GET['password'], $data['motDePasse'])) {
-            $reponse = '1';
-        } else {
-            $reponse = '0';
-        }
-    }
 
-    echo json_encode($reponse);
+    $statement = $pdo->prepare("UPDATE OFFRE SET nbPlaceDisponible = nbPlaceDisponible + " + $data['nbPlaceSouhaitees'] + " WHERE idfOffre = :idfOffre");
+    $statement->bindValue(":idfOffre", $_POST["idfOffre"]);
+    $statement->execute() or die(print_r($statement->errorInfo(), true));
+
+    echo "1";
 }
