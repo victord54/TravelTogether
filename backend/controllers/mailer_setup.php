39ac6058fbc078729b0e45b4fd8f063ae4021983
levathian1 <randomynot02@gmail.com>
2023-03-30 11:42:39 +0200

    updated slashes

--- backend/controllers/mailer_setup.php
+++ backend/controllers/mailer_setup.php
@@ -1,7 +1,7 @@
 <?php
 
-use PHPMailer/PHPMailer/PHPMailer;
-use PHPMailer/PHPMailer/Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 
 //setup liens vers fichiers phpmailer requis
