4cc6177916f4396c7b98519bd9a8d9f019afa6e8
Nathan <nathan.faynot3@etu.univ-lorraine.fr>
2023-04-04 00:10:21 +0200

    Problème à régler : requête ajax qui doit être prioritaire

--- frontend/src/components/Offer.jsx
+++ frontend/src/components/Offer.jsx
@@ -1,133 +1,166 @@
-import React, { useState } from "react";
 import axios from "axios";
-import { url_api } from "../data/url_api";
-import { Link, useNavigate } from "react-router-dom";
+import {url_api} from "../data/url_api";
+import {Link} from "react-router-dom";
 import "../styles/SeeOffer.css";
+import $ from 'jquery';
 
-function Offer(data) {
+function Offer(data)
+{
+  let isCreator = false
+  function cancelReply() //envoie au serv la demande d'annulation de participation à un trajet
+  {
+    console.log("Envoi de l'annulation");
+    const formData = new FormData();
+    formData.append("mail", localStorage.getItem("mail"));
+    formData.append("idfOffre", data["idfOffre"]);
 
-    //const navigate = useNavigate();
+    console.log("formdata: " + data["idfOffre"] + localStorage.getItem("mail"));
 
-    function cancelReply(){  //envoie au serv la demande d'annulation de participation à un trajet
-        
-        console.log("Envoi de l'annulation");
-        const formData = new FormData();
-        formData.append("mail", localStorage.getItem("mail"));
-        formData.append("idfOffre", data["idfOffre"]);
+    axios
+        .post(url_api.url + "/cancel_ride", formData)
+        .then(function(response)
+        {
+          console.log("Response :" + response.data);
+        })
+        .catch(function(error)
+        {
+          console.log("Error :" + error);
+        });
+  }
 
-        console.log("formdata: " + data["idfOffre"] + localStorage.getItem("mail"));
+  var info = <></>;
+  var precision = <></>;
+  var inter = <></>;
+  var annulerParticipation = <></>;
+  var date = new Date(data["dateDepart"]);
+  var hour = new Date("July 1 2023 " + data["heureDepart"]);
+  var annulable = (typeof data["isCancellable"] !== 'undefined'); //affichage du bouton d'annulation de la participation à cette offre
 
-        axios
-            .post(url_api.url + "/cancel_ride", formData)
-            .then(function (response) {
-                console.log("Response :" + response.data);
-                //éventuellement mettre ici des trucs pour afficher une notif
-                //navigate("/profile");
-            })
-            .catch(function (error) {
-                console.log("Error :" + error);
-            });
+  const dateD = new Date(date);
+  dateD.setHours(hour.getHours());
+  dateD.setMinutes(hour.getMinutes());
+  const dateAuj = new Date();
+
+  // axios //On check si l'utilisateur a créé l'offre
+  //   .get(url_api.url + "/cancel_ride", {
+  //     params: {
+  //       mail: localStorage.getItem("mail"),
+  //       idfOffre: data["idfOffre"],
+  //     },
+  //   })
+  //   .then(function(response)
+  //   {
+  //     if(response.data === "0") localStorage.setItem("creator", "true")
+  //   })
+  //   .catch(function(error)
+  //   {
+  //     console.log("Error :" + error);
+  //   })
+
+  $.ajax({
+    url: url_api.url + "/cancel_ride",
+    type : 'GET',
+    data:
+        {
+          'mail': localStorage.getItem("mail"),
+          'idfOffre': data["idfOffre"],
+        },
+    dataType: "json",
+    success: function(reponse)
+    {
+      if(reponse === "0") isCreator = true
+    },
+    error:function(error)
+    {
+      console.log("Error :" + error);
     }
+  });
 
-
-    var info = <></>;
-    var precision = <></>;
-    var inter = <></>;
-    var annulerParticipation = <></>;
-    var date = new Date(data["dateDepart"]);
-    var hour = new Date("July 1 2023 " + data["heureDepart"]);
-    var annulable = (typeof data["isCancellable"] !== 'undefined'); //affichage du bouton d'annulation de la participation à cette offre
-
-    const dateD = new Date(date);
-    dateD.setHours(hour.getHours());
-    dateD.setMinutes(hour.getMinutes());
-    const dateAuj = new Date();
-
-    if(annulable){ //l'offre est annulable, l'utilisateur qui l'annule est celui du localstorage
-        console.log("ZEBIED");
-        annulerParticipation = (
-            <button className="bouton-annuler" onClick={cancelReply}> Annuler participation </button>
-        );
-    }
-
-    if (data["infos"].length > 1)
-        info = (
-            <main>
-                <h3>Informations :</h3>
-                <p>{data["infos"]}</p>
-            </main>
-        );
-    if (data["precisions"].length > 1)
-        precision = (
-            <main>
-                <h3>Précision :</h3>
-                <p>{data["precisions"]}</p>
-            </main>
-        );
-
-    if (data["inter"] && data["inter"].length > 0)
-        inter = (
-            <main>
-                <h3>Arrêts intermédiaires :</h3>
-                <ul className="citiesList">
-                    {data["inter"].map((city, index) => (
-                        <li key={index}>{city}</li>
-                    ))}
-                </ul>
-            </main>
-        );
-
-    var nbPlaceDispo = data["nbPlaceDisponible"] - ( data["nbPlacesReserves"] ? data["nbPlacesReserves"] : 0);
-    var placedispo = (
-        <p>Nombre de places disponibles : {nbPlaceDispo}</p>
+  if(annulable && !isCreator) //l'offre est annulable, l'utilisateur qui l'annule est celui du localstorage
+  {
+    annulerParticipation = (
+        <button className="bouton-annuler" onClick={cancelReply}> Annuler participation </button>
     );
-    if (nbPlaceDispo === 1)
-        placedispo = (
-            <p>Nombre de place disponible : {nbPlaceDispo}</p>
-        );
-    if (nbPlaceDispo === 0)
-        placedispo = <p>Aucune place disponible.</p>;
+  }
 
-    return (
-        <section key={data["idfOffre"]}>
-            <h2>
-                {data["villeDepart"]} &#8594; {data["villeArrivee"]}
-            </h2>
-            <h1>
-                {data["prix"]} €
-            </h1>
-            <img
-                alt="profil"
-                src={data["photo"] + "?" + Math.random()}
-                width="75px"
-            />
-            <h3>{data["nom"] + " " + data["prenom"]}</h3>
-            <p>
-                Le{" "}
-                {date.getDate().toString().padStart(2, "0") +
-                    "/" +
-                    (date.getMonth() + 1).toString().padStart(2, "0") +
-                    "/" +
-                    date.getFullYear()}{" "}
-                à{" "}
-                {hour.getHours() +
-                    "h" +
-                    hour.getMinutes().toString().padStart(2, "0")}
-            </p>
-            {inter}
-            {precision}
-            {info}
-            {dateD < dateAuj ? (
-                <Link to={"/rating-user/" + data["idfOffre"]}>
-                    <button>Noter les participants</button>
-                </Link>
-            ) : (
-                <>{placedispo}{annulerParticipation}</>
-            )}
-        </section>
+  if(data["infos"].length > 1)
+    info = (
+        <main>
+          <h3>Informations :</h3>
+          <p>{data["infos"]}</p>
+        </main>
     );
+
+  if(data["precisions"].length > 1)
+    precision = (
+        <main>
+          <h3>Précision :</h3>
+          <p>{data["precisions"]}</p>
+        </main>
+    );
+
+  if(data["inter"] && data["inter"].length > 0)
+    inter = (
+        <main>
+          <h3>Arrêts intermédiaires :</h3>
+          <ul className="citiesList">
+            {data["inter"].map((city, index) => (
+                <li key={index}>{city}</li>
+            ))}
+          </ul>
+        </main>
+    );
+
+  var nbPlaceDispo = data["nbPlaceDisponible"] - (data["nbPlacesReserves"] ? data["nbPlacesReserves"] : 0);
+  var placedispo = (
+      <p>Nombre de places disponibles : {nbPlaceDispo}</p>
+  );
+  if(nbPlaceDispo === 1)
+    placedispo = (
+        <p>Nombre de place disponible : {nbPlaceDispo}</p>
+    );
+  if(nbPlaceDispo === 0)
+    placedispo = <p>Aucune place disponible.</p>;
+
+  return (
+      <section key={data["idfOffre"]}>
+        <h2>
+          {data["villeDepart"]} &#8594; {data["villeArrivee"]}
+        </h2>
+        <h1>
+          {data["prix"]} €
+        </h1>
+        <img
+            alt="profil"
+            src={data["photo"] + "?" + Math.random()}
+            width="75px"
+        />
+        <h3>{data["nom"] + " " + data["prenom"]}</h3>
+        <p>
+          Le{" "}
+          {date.getDate().toString().padStart(2, "0") +
+              "/" +
+              (date.getMonth() + 1).toString().padStart(2, "0") +
+              "/" +
+              date.getFullYear()}{" "}
+          à{" "}
+          {hour.getHours() +
+              "h" +
+              hour.getMinutes().toString().padStart(2, "0")}
+        </p>
+        {inter}
+        {precision}
+        {info}
+        {dateD < dateAuj ? (
+            <Link to={"/rating-user/" + data["idfOffre"]}>
+              <button>Noter les participants</button>
+            </Link>
+        ) : (
+            <>{placedispo}{annulerParticipation}</>
+        )}
+      </section>
+  );
 }
-
 
 
 export default Offer;
